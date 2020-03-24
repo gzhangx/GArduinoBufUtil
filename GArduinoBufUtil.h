62fd7654dfa87ff424ef322611cbfb7a463bbb19
Gang Zhang <gzhangx@hotmail.com>
2020-03-24 12:05:51 -0400

    'test'

---
+++ GArduinoBufUtil.h
@@ -0,0 +1,37 @@
+const int BT_BUF_LEN=128;
+
+class RecBuf{  
+    char receiveStr[BT_BUF_LEN+16];
+    int receivePos = 0;
+    int cmdNamePos = -1;
+    public:
+    String cmd = ""; 
+    String val = "";
+    String origVal = "";
+    bool onRecv(int c) {
+      if (receivePos < BT_BUF_LEN) {
+          receiveStr[receivePos++] = (char)c;
+          receiveStr[receivePos] = 0;
+          if (c == ':') {
+            cmdNamePos = receivePos;
+          }
+          if (c== '\n') {
+            receiveStr[receivePos-1]=0;
+            if (receivePos >= 2 && receiveStr[receivePos-2] =='\r')receiveStr[receivePos-2]= 0;
+            receivePos = 0;
+            origVal = receiveStr;
+            if (cmdNamePos >=0) {
+              cmd = origVal.substring(0,cmdNamePos-1);
+              val = origVal.substring(cmdNamePos);
+            }else {
+              val = origVal;
+            }
+            cmdNamePos = -1;
+            return true;
+          }
+        }else {
+          receivePos = 0; //warning, over flow
+        }
+        return false;
+    }
+};
