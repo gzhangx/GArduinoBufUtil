97997fbcfe487c93fb0784ed2e07480436637562
Gang Zhang <gzhangx@hotmail.com>
2020-03-25 15:19:41 -0400

    adding other term

--- GArduinoBufUtil.h
+++ GArduinoBufUtil.h
@@ -8,14 +8,14 @@ class RecBuf{
     String cmd = ""; 
     String val = "";
     String origVal = "";
-    bool onRecv(int c) {
+    bool onRecv(int c, int otherTerm =0) {
       if (receivePos < BT_BUF_LEN) {
           receiveStr[receivePos++] = (char)c;
           receiveStr[receivePos] = 0;
           if (c == ':') {
             cmdNamePos = receivePos;
           }
-          if (c== '\n') {
+          if (c== '\n' || c == otherTerm) {
             receiveStr[receivePos-1]=0;
             if (receivePos >= 2 && receiveStr[receivePos-2] =='\r')receiveStr[receivePos-2]= 0;
             receivePos = 0;
